495655ad2f011d99f21927b4430cbb2e4415f066
Gokcan Parlaktas <gokcanparlaktas@hotmail.com>
2024-12-05 07:49:52 +0300

    Profile section added,css improved

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,10 @@ const Hero = () => {
   const { language, toggleLanguage, localizedData } = useContext(DataContext);
 
   return (
-    <div className="pt-5 pb-20">
-      <div className="container ">
+    <div className="pt-5 relative pb-20 bg-gray-100">
+      <div className="w-[13rem] bg-[#EA2678] h-[3.6875rem] absolute bottom-[5rem] right-[-5rem] rounded-[1.84375rem] opacity-90 "></div>
+      <div className="w-[111px] h-[111px] top-[-4rem] left-[26rem] absolute bg-gray-300 rounded-full"></div>
+      <div className="container relative ">
         <div className="flex justify-end text-neutral-500 font-bold tracking-[0.1rem] text-base">
           <div>
             {language === "en-EN" ? (
@@ -32,14 +34,15 @@ const Hero = () => {
             )}
           </div>
         </div>
-        <div className="flex sm:ml-5  place-content-around mt-32 flex-wrap-reverse lg:flex-nowrap gap-5">
-          <article className="flex flex-col gap-5">
+        <div className="flex sm:ml-5 relative place-content-around mt-32 flex-wrap-reverse lg:flex-nowrap gap-5">
+          <article className="flex relative z-10 flex-col gap-5">
             <div className="text-3xl font-normal leading-9">
               {localizedData.greet}
             </div>
-            <div className="text-[42px] max-w-3xl font-medium leading-[64px]">
+            <div className="text-[42px] max-w-3xl font-medium leading-[64px] z-20">
               {localizedData.article}
             </div>
+            <div className="w-[148px] h-[31px] absolute top-[6rem] left-0 rounded-[4px] bg-[#E92577]"></div>
             <div className="flex gap-5">
               <a
                 href="https://www.linkedin.com/in/gokcanparlaktas96/"
@@ -72,6 +75,8 @@ const Hero = () => {
             />
           </div>
         </div>
+
+        <div className="w-[121px] h-[121px] border-[20px] border-gray-300 rounded-full absolute lg:right-[14rem] lg:bottom-[-9rem] sm:bottom-[-8rem] sm:right-[4rem] opacity-50"></div>
       </div>
     </div>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,13 +5,13 @@ export const Profile = () => {
   const { localizedData } = useContext(DataContext);
   const info = localizedData.info;
   return (
-    <div className=" mt-5 pb-20 ">
+    <div className=" mt-5 pb-20 bg-gray-100 ">
       <div className="flex flex-col container ">
         <div className="mx-auto mt-10 ">
           <h2 className="text-4xl mb-10">{localizedData.profileHeader}</h2>
         </div>
         <div className="flex container flex-wrap lg:flex-nowrap md:gap-24 place-content-around gap-5">
-          <div className="my-auto md:min-w-80 max-w-lg rounded-2xl px-10 py-10 mx-auto shadow-[10px_10px_2px_rgb(82,82,82)]">
+          <div className="my-auto md:min-w-80 max-w-lg rounded-2xl px-10 py-10 mx-auto shadow-[10px_10px_2px_rgb(82,82,82)] bg-white">
             <h3 className="font-light mb-5 text-2xl font-playfair text-[#EA2678]">
               {localizedData.profile}
             </h3>
